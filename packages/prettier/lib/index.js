d538d3844d9ca0742ac8a6b2958fb0712035cf10
maomao <1714487678@qq.com>
2023-03-15 20:01:13 +0800

    feat(prettier): 添加新的配置规则

--- packages/prettier/lib/index.js
+++ packages/prettier/lib/index.js
@@ -25,10 +25,23 @@ module.exports = {
   bracketSameLine: false,
   // 箭头函数仅有一个参数时，参数也添加括号 (x) => x
   arrowParens: 'always',
+  // 每个文件格式化的范围是文件的全部内容
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  // 对所有文件进行格式化，而不是只对在开头含有特定注释（@prettier 或 @format）的文件进行格式化
+  requirePragma: false,
+  // 格式化的同时自动插入 @format 的特殊注释（表示该文件已被格式化）
+  insertPragma: false,
   // 对 Markdown 文本换行不进行任何操作，保持原样
   proseWrap: 'preserve',
   // 对 HTML 全局空白不敏感
   htmlWhitespaceSensitivity: 'ignore',
   // 不对 vue 中的 script 及 style 标签进行缩进
-  vueIndentScriptAndStyle: false
+  vueIndentScriptAndStyle: false,
+  // 换行符使用 lf
+  endOfLine: 'lf',
+  // 自动格式化嵌入的代码内容
+  embeddedLanguageFormatting: 'auto',
+  // 不强制 html vue jsx 中的属性（具有多个时）单独占一行
+  singleAttributePerLine: false
 }
