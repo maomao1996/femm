407efe65d693ae714d82916cb70323d01b49cb2a
maomao <1714487678@qq.com>
2023-04-11 19:03:09 +0800

    test(tailwind): 添加测试用例

---
+++ packages/tailwind-config/tests/config.test.js
@@ -0,0 +1,28 @@
+const postcss = require('postcss')
+
+it('tailwindcss presets config', () => {
+  /* 编译出来的缩进是 4 空格 */
+  const expected = `
+.z-\\[1996\\] {
+    z-index: 1996
+}
+.flex {
+    display: flex
+}
+.flex-row-center {
+    display: flex;
+    justify-content: center;
+    align-items: center
+}
+`
+  postcss([
+    require('tailwindcss')({
+      content: [{ raw: `flex flex-row-center z-[1996]` }],
+      presets: [require('../lib/index')]
+    })
+  ])
+    .process('@tailwind utilities', { from: undefined })
+    .then(({ css }) => {
+      expect(css).toBe(expected.trim())
+    })
+})
---
+++ packages/tailwind-config/tests/flex-center.test.js
@@ -0,0 +1,28 @@
+const postcss = require('postcss')
+
+it('tailwindcss plugins flex-center', () => {
+  /* 编译出来的缩进是 4 空格 */
+  const expected = `
+.flex-row-center {
+    display: flex;
+    justify-content: center;
+    align-items: center
+}
+.flex-col-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column
+}
+`
+  postcss([
+    require('tailwindcss')({
+      content: [{ raw: 'flex-row-center flex-col-center' }],
+      plugins: [require('../lib/plugins/flex-center')]
+    })
+  ])
+    .process('@tailwind utilities', { from: undefined })
+    .then(({ css }) => {
+      expect(css).toBe(expected.trim())
+    })
+})
